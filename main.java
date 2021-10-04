7a6441354a046c77f4e91e3e5c918e35e890e3d2
figielekr <48886494+figielekr@users.noreply.github.com>
2021-10-04 13:23:06 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) /*throws InterruptedException*/ {
         //account Kate = new account("Kate",500500, 6000);
         //account Tim = new account("Tim",222222, 6500);
         //Scanner scanner = new Scanner(System.in);
@@ -9,6 +9,14 @@ public class main {
 
         //account.menu();
         //System.out.println(account.name+" wplacono: " + account.money);
+/*        System.out.println("System is starting...");
+        Thread.sleep(250);
+        System.out.println(".");
+        Thread.sleep(400);
+        System.out.println("..");
+        Thread.sleep(400);
+        System.out.println("...");
+        Thread.sleep(450);*/
         account.menu();
 
     }
