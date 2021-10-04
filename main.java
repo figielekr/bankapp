3bce1983570550b7b22f1aa621d7cdfcc7857cde
figielekr <48886494+figielekr@users.noreply.github.com>
2021-10-04 11:46:14 +0200

    main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        //account Kate = new account("Kate",500500, 6000);
+        //account Tim = new account("Tim",222222, 6500);
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Zakladamy konto, podaj nick uzytkownika: ");
+
+        //account.menu();
+        //System.out.println(account.name+" wplacono: " + account.money);
+        account.menu();
+
+    }
+}
